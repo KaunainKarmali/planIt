2d6afebb7b2511b681ec8825355d491f8c574730
Kaunain Karmali <Kaunain.Karmali@ca.ey.com>
2021-04-30 17:48:41 -0400

    added back-end feature to create and save notes to database

--- server/controllers/trial.js
+++ server/controllers/trial.js
@@ -4,121 +4,46 @@ import User from "../models/user.js";
 export async function createNote(req, res) {
   const { ip, note, board } = req.body;
 
-  console.log(ip);
-  console.log(note);
-  console.log(board);
+  let query = {
+    $and: [{ ip: ip }, { data: { $elemMatch: { board: board } } }],
+  };
 
-  // const user =
-  // console.log(user);
+  let update = { $push: { "data.$.notes": note } };
 
-  // if (user.data[board]) {
-  // console.log("board found");
-  // user.data[board].push(note);
-  // } else {
-  // console.log("board NOT found");
-  // console.log(user.data);
-
-  // const updateDocument = { $set: { data: { $push: { [board]: note } } } };
-
-  // const updateDocument = {
-  //   $set: { (`data.${board}`) : { $push: { [board]: note } } },
-  // };
-
-  // const updateDocument = {
-  //   $set: { data: { $push: { [board]: note } } },
-  // };
-
-  const key = `data.${board}`;
-
-  // const updateDocument = {
-  //   $set: { [key]: note },
-  // };
-
-  // const newBoard = { [board]: [note] };
-  // const updateBoard = { $push: { [board]: note } };
-
-  // const updateDocument = {
-  //   $set: {
-  //     `data.$[elem].${board}` : note
-  //   }
-  // };
-
-  // const query = { ip: ip, data: board };
-
-  // const resultOne = await User.updateOne(
-  //   query,
-  //   updateBoard,
-  //   (err, response) => {
-  //     console.log(response);
-  //   }
-  // );
-  // console.log("RESPONSE ONE...");
-  // console.log(resultOne);
-
-  // const queryTwo = { ip: ip };
-
-  // const updateDocumentTwo = { $set: { data: newBoard } };
-  // // Update user model when the board is new
-  // const result = await User.updateOne(
-  //   queryTwo,
-  //   updateDocumentTwo,
-  //   (err, response) => {
-  //     // console.log(response);
-  //   }
-  // );
-  // console.log("RESPONSE TWO...");
-  // console.log(result);
-
-  // if (user.data[board]) {
-  //   console.log("Cannot find board");
-  // } else {
-  //   console.log("Found board");
-  //   const result = await user.updateOne(updateDocument, (err, response) => {
-  //     console.log(response);
-  //   });
-  //   console.log(result);
-  // }
-
-  // user.update({
-  //   $push: { data: newBoard },
-  // });
-
-  // console.log();
-  // document.updateOne({ $push: { data: note } }, (err, response) => {
-  //   console.log(response);
-  // });
-  // }
-
-  // const updateDocument = {
-  //   $push: { notes: note },
-  // };
-
-  // const updateDocument = {
-  //   $push: {
-  //     data: {
-  //       [board]: note,
-  //     },
-  //   },
-  // };
+  let options = {
+    useFindAndModify: false,
+    new: true,
+    rawResult: true,
+  };
 
   try {
-    // const result = await User.updateOne(query, updateDocument);
-    // result.then((res) => console.log(res));
-    // // Saved successfully
-    // if ((result.ok === 1) & (result.nModified === 1)) {
-    //   res.status(201).json({ message: "Saved successfully." });
-    // }
-    // // User cannot be found
-    // else if ((result.ok === 1) & (result.nModified === 0)) {
-    //   res
-    //     .status(201)
-    //     .json({ message: "Save unsuccessful. User cannot be found." });
-    // }
-    // // Error, could not save
-    // else {
-    //   res.status(201).json({ message: result });
-    // }
+    // Append new note to the existing user's data
+    let status = await User.findOneAndUpdate(query, update, options).exec();
+
+    // Create a new board if the board does not exist for the user
+    if (status.value === null) {
+      query = { ip: ip };
+      update = { $push: { data: { board: board, notes: [note] } } };
+      status = await User.findOneAndUpdate(query, update, options).exec();
+    }
+
+    // Provide response depending on creation status
+
+    // Saved successfully
+    if ((status.value !== null) & (status.ok === 1)) {
+      res.status(201).json({ message: status.value });
+    }
+
+    // Cannot save
+    else if (status.lastErrorObject.n === 0) {
+      res.status(201).json({ message: "Cannot save note." });
+    }
+
+    // All other errors
+    else {
+      res.status(409).json({ message: status });
+    }
   } catch (error) {
-    // res.status(409).json({ message: error.message });
+    res.status(409).json({ message: error });
   }
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,29 @@
 import mongoose from "mongoose";
 
+// Note schema
+const noteSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+// Board schema
+const boardSchema = new mongoose.Schema({
+  board: {
+    type: String,
+  },
+  notes: {
+    type: [noteSchema],
+  },
+});
+
+// User schema
 const userSchema = new mongoose.Schema({
   ip: String,
   creationDate: {
     type: Date,
     default: new Date(),
   },
-  data: {},
+  data: [boardSchema],
 });
 
 const User = mongoose.model("User", userSchema);
