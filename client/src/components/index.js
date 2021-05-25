580c94a00701fe6cff69de5fb0ee707559eb6953
Kaunain <kaunaink@gmail.com>
2021-05-24 22:37:59 -0400

    projects overview added to dashboard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,13 @@ import Footer from "./Footer/Footer";
 import ProjectMain from "./ProjectMain/ProjectMain";
 import CloseConfirmationDialog from "./CloseConfirmationDialog/CloseConfirmationDialog";
 import CreateNewItem from "./CreateNewItem/CreateNewItem";
+import DashboardMain from "./DashboardMain/DashboardMain";
 
-export { Header, Footer, ProjectMain, CloseConfirmationDialog, CreateNewItem };
+export {
+  Header,
+  Footer,
+  ProjectMain,
+  CloseConfirmationDialog,
+  CreateNewItem,
+  DashboardMain,
+};
